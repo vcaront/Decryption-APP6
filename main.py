3c839e291de7abf8d1d4cc885e88561772f8f35e
William Roberge <202059891@edu.clg.qc.ca>
2023-07-26 10:46:41 -0400

    gide line

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+
+
+
+
 if __name__ == '__main__':
     # coucou
     print("hello world")
-    
+    #étape1 trouver P,Q de N et le e tout en haut avec polar
+    #étape2 trouver phi de n = (p-1)(q-1)
+    #étape3 trouver d d = inverse e ....
+    #étape4 décryper G P Q avec le d trouver
+    #note g est potentiellment mauvais
+    #étape5 trouver xy de G^xy 0,1,-1 peut-être
+    #étape6 déchiffre le document par flux
+    #étape7 décryperter le salaire avec le N et e publique avec la méthode. méthode p-1
